0acd443775f4b569c8c53ad2dc15ebb779263731
dylananderton <66233295+dylananderton@users.noreply.github.com>
2022-07-16 20:08:00 +0800

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,98 +1,142 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-import NotesList from './Notes/NotesList';
-import Search from './Notes/Search';
-import {BsFillMoonStarsFill, BsFillSunFill} from 'react-icons/bs'
-import UseDarkMode from './Notes/UseDarkMode';
+import React, { useState } from 'react'
+import "./App.css"
+import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from './PassGen/Characters'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { COPY_Fail, COPY_SUCCESS } from './PassGen/Message';
 
 const App = () => {
-	const [notes, setNotes] = useState([
-		{
-			id: nanoid(),
-			text: 'This is my first note!',
-			date: '05/05/2022',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my second note!',
-			date: '06/05/2022',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my third note!',
-			date: '15/05/2022',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my new note!',
-			date: '25/05/2022',
-		},
-	]);
+  const [password, setPassword] = useState("")
+  const [passwordLength, setPasswordLength] = useState(26)
+  const [includeUpperCase, setIncludeUpperCase] = useState(false)
+  const [includeLowerCase, setIncludeLowerCase] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+  const handleGeneratePassword = () => {
+    if (!includeUpperCase && !includeLowerCase && !includeNumbers && !includeSymbols) {
+      notify("To generate password you must select atleast one checkbox", true)
+    }
+    else {
+      let characterList = ""
+      if (includeNumbers) {
+        characterList = characterList + numbers
+      }
+      if (includeUpperCase) {
+        characterList = characterList + upperCaseLetters
+      }
+      if (includeLowerCase) {
+        characterList = characterList + lowerCaseLetters
+      }
+      if (includeSymbols) {
+        characterList = characterList + specialCharacters
+      }
+      setPassword(createPassword(characterList))
+      notify("Password is generated successfully", false)
+    }
 
-	const [searchText, setSearchText] = useState('');
 
-	useEffect(() => {
-		const savedNotes = JSON.parse(
-			localStorage.getItem('react-notes-app-data')
-		);
+  }
+  const createPassword = (characterList) => {
+    let password = ""
+    const characterListLength = characterList.length
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength)
+      password = password + characterList.charAt(characterIndex)
+    }
+    return password
+  }
+  const copyToClipboard = (password) => {
 
-		if (savedNotes) {
-			setNotes(savedNotes);
-		}
-	}, []);
+    navigator.clipboard.writeText(password)
+  }
+  const notify = (message, hasError = false) => {
+    if (hasError) {
+      toast.error(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    else {
+      toast(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
 
-	useEffect(() => {
-		localStorage.setItem(
-			'react-notes-app-data',
-			JSON.stringify(notes)
-		);
-	}, [notes]);
+  }
+  const handleCopyPassword = (e) => {
+    if (password === "") {
+      notify(COPY_Fail, true)
+    }
+    else {
+      copyToClipboard(password)
+      notify(COPY_SUCCESS)
+    }
 
-	const addNote = (text) => {
-		const date = new Date();
-		const newNote = {
-			id: nanoid(),
-			text: text,
-			date: date.toLocaleDateString(),
-		};
-		const newNotes = [...notes, newNote];
-		setNotes(newNotes);
-	};
+  }
 
-	const deleteNote = (id) => {
-		const newNotes = notes.filter((note) => note.id !== id);
-		setNotes(newNotes);
-	};
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator__header">
+           Password Generator
+          </h2>
+          <div className="generator__password">
+            <h3 >{password}</h3>
+            <button className="copy__btn">
+              <i onClick={handleCopyPassword} className="far fa-clipboard"></i>
+            </button>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-strength">Password length</label>
+            <input className="pw" defaultValue={passwordLength} onChange={(e) => setPasswordLength(e.target.value)} type="number" id="password-stregth" name="password-strength" max="26" min="8" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="uppercase-letters">Add Uppercase Letters</label>
+            <input checked={includeUpperCase} onChange={(e) => setIncludeUpperCase(e.target.checked)} type="checkbox" id="uppercase-letters" name="uppercase-letters" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lowercase-letters">Add Lowercase Letters</label>
+            <input checked={includeLowerCase} onChange={(e) => setIncludeLowerCase(e.target.checked)} type="checkbox" id="lowercase-letters" name="lowercase-letters" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-numbers">Include Numbers</label>
+            <input checked={includeNumbers} onChange={(e) => setIncludeNumbers(e.target.checked)} type="checkbox" id="include-numbers" name="include-numbers" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-symbols">Include Symbols</label>
+            <input checked={includeSymbols} onChange={(e) => setIncludeSymbols(e.target.checked)} type="checkbox" id="include-symbols" name="include-symbols" />
+          </div>
+          <button onClick={handleGeneratePassword} className="generator__btn">
+            Generate Password
+          </button>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </div>
+      </div>
+      <a className='source-code' href='https://github.com/dylananderton/Password-Generator' target='_blank'><p className='source-code-text'>See the source code</p></a>
+    </div>
+  )
+}
 
-	const [isDarkMode,setDarkMode] = UseDarkMode()
-
-	return (
-    	<div>
-			<div className='container'>
-				<div className='header'>
-					<h1 style={{color: isDarkMode?'#FAF9F6':'#28282B'}}><span className='my' style={{color: isDarkMode?'aquamarine':'#289D8C'}}>My</span>&nbsp;Notes</h1>
-					<button className="toggle_btn" onClick={()=>setDarkMode(!isDarkMode)} style={{backgroundColor: isDarkMode?'#28282B':'#FAF9F6'}}>
-						{isDarkMode? (
-							<BsFillSunFill size={'2em'} title="Switch to light mode" style={{color:"white"}}/>
-						) : (
-							<BsFillMoonStarsFill size={'2em'} title="Switch to dark mode" />
-						)}
-					</button>
-				</div>
-				<Search handleSearchNote={setSearchText} />
-				<NotesList
-					notes={notes.filter((note) =>
-						note.text.toLowerCase().includes(searchText)
-					)}
-					handleAddNote={addNote}
-					handleDeleteNote={deleteNote}
-				/>
-			</div>
-			<footer>
-				<a className='source-code' style={{color: isDarkMode?'rgb(233, 233, 233)':'#28282B'}} href="https://github.com/dylananderton/Notes" target="_blank" >See the source code</a>
-			</footer>
-		</div>
-	);
-};
-
-export default App;
+export default App
